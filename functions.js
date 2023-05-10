ea7c9720e5dd7e03634a6df62d4946ef781f3cce
nbtruman <104936442+nbtruman@users.noreply.github.com>
2023-05-10 13:42:11 +0100

    DRY

--- functions.js
+++ functions.js
@@ -1,11 +1,14 @@
 const testForm = document.getElementById("testForm");
-const boxOne = document.getElementById("boxOne");
-const boxTwo = document.getElementById("boxTwo");
-const boxThree = document.getElementById("boxThree");
+const statuses = [
+    document.getElementById("boxOne"),
+    document.getElementById("boxTwo"),
+    document.getElementById("boxThree")
+];
+
 function submit(){
     testForm.submit();
 }
 
-boxOne.addEventListener("change", submit);
-boxTwo.addEventListener("change", submit);
-boxThree.addEventListener("change", submit);
+statuses.forEach(element => {
+    element.addEventListener("change", submit);
+});
